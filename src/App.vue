f8ede3a5feff4b24893a448861f36bc28f8d2d8c
DouglasGuacaran <douglasguacaran@gmail.com>
2021-10-04 10:06:22 -0300

    Recarga Repositorio
    Carga de proyecto

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <b-navbar toggleable="lg">
+        <b-navbar-brand class="justify-content-center" href="#"
+          ><router-link to="/"
+            ><img alt="LogoPokedex" src="../public/img/logoPokedex.png" />
+          </router-link>
+        </b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-navbar-brand href="#">
+              <router-link to="/atraparPokemon">Atrapar Pokémon </router-link>
+            </b-navbar-brand>
+            <b-navbar-brand
+              class="colorLink"
+              v-for="(el, index) in arrayRutas"
+              :key="index"
+            >
+              <router-link :to="'/pokemonPorHabilidad' + el.path"
+                ><u> {{ el.value }}</u></router-link
+              >
+            </b-navbar-brand>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["arrayRutas"]),
+  },
+};
+</script>
+
+<style lang="scss">
+@import url(//db.onlinewebfonts.com/c/f4d1593471ddebd973210265762a?family=Pokemon);
+//@import url('https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@300&display=swap");
+
+.navbar {
+  background-color: #ffffff52;
+  border: solid rgba(88, 109, 129, 0.801);
+  border-radius: 5px;
+  font-family: "Source Code Pro", monospace;
+  //font-family: 'Courier Prime', monospace;
+}
+
+#app {
+  font-family: "Source Code Pro", monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+  background-image: url(../public/img/FondoPokedex.jpg);
+  background-repeat: no-repeat;
+  background-color: rgb(127, 172, 113);
+}
+html {
+  background-image: url(../public/img/FondoPokedex.jpg);
+  background-repeat: no-repeat;
+}
+#nav {
+  padding: 30px;
+
+  a {
+    &.router-link-exact-active {
+      color: rgba(88, 109, 129, 0.801);
+    }
+  }
+}
+
+u {
+  color: rgba(88, 109, 129, 0.801);
+}
+.navbar-toggler {
+  display: flex;
+  justify-content: flex-end;
+  margin-left: 5px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/Pokebola.png" width="100px" /><br />
+    <HelloWorld msg="Bienvenidos a mi App de Pokédex" /><br />
+    <h2><strong>Podrás atrapar Todos los Pokémons que desees!</strong></h2>
+    <br />
+    <h3><strong>Anímate dale Play!</strong></h3>
+    <br />
+    <audio controls>
+      <source src="../assets/mp3/Pokemon.mp3" type="audio/mp3" />
+    </audio>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+audio {
+  border: solid red;
+  border-radius: 30px;
+}
+</style>
