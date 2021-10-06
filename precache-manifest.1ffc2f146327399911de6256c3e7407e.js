b1f96eea48366b50683e0b218ba2dcbe913ca399
DouglasGuacaran <douglasguacaran@gmail.com>
2021-10-06 11:54:40 -0300

    deploy (commit: f8ede3a5feff4b24893a448861f36bc28f8d2d8c)

---
+++ precache-manifest.1ffc2f146327399911de6256c3e7407e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0b9154e046e28275bb",
+    "url": "/vue-pages-pokedex/css/app.44b29d92.css"
+  },
+  {
+    "revision": "0aff230f781cb68da790",
+    "url": "/vue-pages-pokedex/css/chunk-vendors.25ab3afa.css"
+  },
+  {
+    "revision": "9813a8f8734acc82cd582de677356c37",
+    "url": "/vue-pages-pokedex/img/FondoPokedex.9813a8f8.jpg"
+  },
+  {
+    "revision": "9813a8f8734acc82cd582de677356c37",
+    "url": "/vue-pages-pokedex/img/FondoPokedex.jpg"
+  },
+  {
+    "revision": "571cb6e431a155c069a008d681215359",
+    "url": "/vue-pages-pokedex/img/Pokebola.571cb6e4.png"
+  },
+  {
+    "revision": "f676ff9b7a04d3375a1e9a318c659999",
+    "url": "/vue-pages-pokedex/img/PokebolaGif.gif"
+  },
+  {
+    "revision": "25a8f165b1ebc87a0ddc477ee0278824",
+    "url": "/vue-pages-pokedex/img/logoPokedex.25a8f165.png"
+  },
+  {
+    "revision": "25a8f165b1ebc87a0ddc477ee0278824",
+    "url": "/vue-pages-pokedex/img/logoPokedex.png"
+  },
+  {
+    "revision": "cb6314fbd889b8c93915a7ceba3ac702",
+    "url": "/vue-pages-pokedex/index.html"
+  },
+  {
+    "revision": "6d0b9154e046e28275bb",
+    "url": "/vue-pages-pokedex/js/app.4b06105a.js"
+  },
+  {
+    "revision": "0aff230f781cb68da790",
+    "url": "/vue-pages-pokedex/js/chunk-vendors.b3d0e675.js"
+  },
+  {
+    "revision": "b3eddef0bf0024280e013149d07be773",
+    "url": "/vue-pages-pokedex/manifest.json"
+  },
+  {
+    "revision": "70afa3a0083a25c4ff50da5a6d9cb53e",
+    "url": "/vue-pages-pokedex/media/Pokemon.70afa3a0.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-pages-pokedex/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/vue-pages-pokedex/precache-manifest.1ffc2f146327399911de6256c3e7407e.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "pokedex_vue_app"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
